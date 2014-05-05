aa95d859e5abd98c17e66b02edc55bba498d9a2c
Jasper Woudenberg <jasper.woudenberg@icemobile.com>
2014-05-05 12:03:20 +0200

    Added support for transform, respond and error handlers.

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 var restify = require('restify');
 module.exports = function(options) {
     var virgilio = this;
+    var port = (options.http && options.http.port) || '8080';
     var Promise = virgilio.Promise;
     var server = restify.createServer();
-    var httpMethods = ['get', 'post', 'put', 'delete', 'head'];
+    var httpMethods = ['get', 'post', 'put', 'del', 'head'];
 
-    virgilio.namespace('http')
+    virgilio = virgilio.namespace('http')
         .defineAction('registerRoutes', registerRoutes)
         .defineAction('registerMiddleware', registerMiddleware);
+
+    virgilio.baseVirgilio$.http = function(routeObject, basePath) {
+        virgilio.execute('registerRoutes', routeObject, basePath);
+    };
+
+    server.listen(port, function() {
+        virgilio.log.info('Http server listening on port: %s', port);
+    });
 
     function registerRoutes(routeObject, basePath) {
         basePath = sanitizePath(basePath || '/');
         var routes = Object.keys(routeObject);
         routes.forEach(function(key) {
-            key = key.toLowerCase();
             var route = routeObject[key];
+            key = key.toLowerCase();
             if (httpMethods.indexOf(key) >= 0) {
                 registerRoute(basePath, key, route);
             }
@@ -32,17 +41,58 @@ module.exports = function(options) {
     }
 
     function createHandler(handlerObject) {
+        handlerObject = extendHandlerObject(handlerObject);
+
+        var handler = function(req, res, next) {
+            var handlerObject = this;
+            Promise.cast(req)
+                .then(handlerObject.transform)
+                .then(handlerObject.handler)
+                .then(function(response) {
+                    return handlerObject.respond(response, res);
+                })
+                .catch(function(error) {
+                    return handlerObject.error(error, res);
+                })
+                .catch(function(error) {
+                    //If the user-defined error handler fails, use the
+                    //default handler.
+                    return handlerObject.error(error, res);
+                }).done();
+        }.bind(handlerObject);
+
+        return handler;
+    }
+
+    function extendHandlerObject(handlerObject) {
         if (typeof handlerObject === 'string') {
             handlerObject = {
                 handler: handlerObject,
             };
         }
-        var handler = function(req, res, next) {
-            var virgilio = this;
-            virgilio.execute(handlerObject.handler);
-        }.bind(virgilio);
+        var handler = handlerObject.handler;
+        handlerObject.handler = function(args) {
+            args = [handler].concat(args);
+            return virgilio.execute.apply(virgilio, args);
+        };
+        handlerObject.transform = handlerObject.transform || defaultTransform;
+        handlerObject.respond = handlerObject.respond || defaultRespond;
+        handlerObject.error = handlerObject.error || defaultError;
+        handlerObject.fallbackError = defaultError;
+        return handlerObject;
+    }
 
-        return handler;
+    function defaultTransform(req) {
+        return req.body;
+    }
+
+    function defaultRespond(response, res) {
+        res.send(200, response);
+    }
+
+    function defaultError(error, res) {
+        virgilio.log.error(error);
+        res.send(500, 'An error occured');
     }
 
     function sanitizePath(path) {
